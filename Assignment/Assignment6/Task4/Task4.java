ffbb7f53c3e84e2ccdff1e44313cc9a9395c5011
HuyHieu <huyhieu.hust2855@gmail.com>
2023-05-27 02:08:53 +0700

    [AS6][T4] cpl

---
+++ Assignment/Assignment6/Task4/Task4.java
@@ -0,0 +1,59 @@
+package Assignment6.Task4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Task4 {
+    public static void main(String[] args){
+        String fileName = "Salary.txt";
+        List<Employee> employees = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] data = line.split("\\s+");
+                String fisrtName1 = data[0].trim();
+                String lastName1 = data[1].trim();
+                String rank1 = data[2].trim();
+                Double salary1 = Double.parseDouble(data[3].trim());
+                Employee employee = new Employee(fisrtName1, lastName1, rank1, salary1);
+                employees.add(employee);
+
+            }
+
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Collections.sort(employees, Comparator.comparingDouble(Employee::getSalary));
+
+        // Hiển thị danh sách đã sắp xếp
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    static class Employee{
+        private String firstName;
+        private String lastName;
+        private String rank;
+        private Double salary;
+
+        public Employee(String firstName, String lastName, String rank, Double salary) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.rank = rank;
+            this.salary = salary;
+        }
+        public double getSalary() {
+            return salary;
+        }
+        @Override
+        public String toString(){
+            return firstName + " " + lastName + " " + rank + " "+ salary;
+        }
+    }
+}
