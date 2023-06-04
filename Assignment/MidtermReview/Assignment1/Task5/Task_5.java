2db5642cf6a274e17bf85cfc80e8b47980e253f4
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-04 16:24:33 +0700

    [AS1] [Task5] finish

--- Assignment/MidtermReview/Assignment1/Task5/Task_5.java
+++ Assignment/MidtermReview/Assignment1/Task5/Task_5.java
@@ -1,4 +1,13 @@
 package MidtermReview.Assignment1.Task5;
 
+import java.util.Random;
+
 public class Task_5 {
+    public static  void main(String[] args){
+        Random random = new Random(1000);
+        for(int i=0; i<50; i++) {
+            int randomNumber = random.nextInt(100);
+            System.out.println(randomNumber);
+        }
+    }
 }
