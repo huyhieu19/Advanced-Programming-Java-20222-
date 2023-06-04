af6c6c9e633bc30bccd1a790b8c8e95ada7eab69
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-04 16:37:03 +0700

    [AS1] [Task6] finish

--- Assignment/MidtermReview/Assignment1/Task6/Stopwatch.java
+++ Assignment/MidtermReview/Assignment1/Task6/Stopwatch.java
@@ -1,4 +1,42 @@
 package MidtermReview.Assignment1.Task6;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Stopwatch {
+    public Instant startTime;
+    public Instant endTime;
+
+    public Instant getEndTime() {
+        return endTime;
+    }
+    public Instant getStartTime(){
+        return startTime;
+    }
+
+    public void setStartTime(Instant startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(Instant endTime) {
+        this.endTime = endTime;
+    }
+
+    public Stopwatch(){
+        startTime = Instant.now();
+    }
+    public void start(){
+        startTime = Instant.now();
+    }
+    public void stop(){
+        endTime = Instant.now();
+    }
+    public long getElapsedTime(){
+        if(startTime != null && endTime != null){
+            Duration duration  = Duration.between(startTime, endTime);
+            return duration.toMillis();
+        }else{
+            return 0;
+        }
+    }
 }
