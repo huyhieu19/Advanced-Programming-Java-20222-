97b77df0efa682531da258aeffb3f82d1ffd5eac
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-04 16:18:20 +0700

    [AS1] [Task4] finish

--- Assignment/MidtermReview/Assignment1/Task4/Task_4.java
+++ Assignment/MidtermReview/Assignment1/Task4/Task_4.java
@@ -1,4 +1,13 @@
 package MidtermReview.Assignment1.Task4;
 
+import java.util.Date;
+
 public class Task_4 {
+    public static  void main(String[] args){
+        long a = 1000;
+        for(int i=0; i<8;i++){
+            Date date = new Date(a = a*10);
+            System.out.println(date.toString());
+        }
+    }
 }
