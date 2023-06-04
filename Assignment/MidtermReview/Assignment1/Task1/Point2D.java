58cf15f10e3326b94a67b18d46c8fb3372fc929f
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-04 13:22:41 +0700

    [AS1] [Task1] complete

--- Assignment/MidtermReview/Assignment1/Task1/Point2D.java
+++ Assignment/MidtermReview/Assignment1/Task1/Point2D.java
@@ -1,5 +1,28 @@
 package MidtermReview.Assignment1.Task1;
 
 public class Point2D {
-
+    double x;
+    double y;
+    // Constructor
+    public Point2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    // Distance
+    public double Distance(double x, double y){
+        return (Math.sqrt(x*x + y*y));
+    }
+    public double Distance(Point2D point){
+        return (Math.sqrt(Math.pow(point.x, 2) + Math.pow(point.y, 2)));
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    @Override
+    public String toString(){
+        return "Toa do: (x, y) = (" + getX() + "," + getY() + ")" +"Distance: x= 3 va y= 4 :" +  Distance(new Point2D(3, 4));
+    }
 }
