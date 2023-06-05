c320f8f01f0e2400dde30fbff0dd9dcd1f186e42
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-05 14:01:01 +0700

    [As4] [Task4] create class circle

--- Assignment/MidtermReview/Assignment4/Task4/Circle.java
+++ Assignment/MidtermReview/Assignment4/Task4/Circle.java
@@ -12,5 +12,26 @@ public class Circle extends GeometricObject{
         super(color, filter);
         this.radius = radius;
     }
-
+    public double getRadius(){
+        return radius;
+    }
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    public double getArea(){
+        return 2*3.14*Math.pow(radius, 2);
+    }
+    public double getPerimeter(){
+        return 2*3.14*this.radius;
+    }
+    public double getDiameter(){
+        return 2*this.radius;
+    }
+    public void PrintCircle(){
+        System.out.println("Circle: \n" +
+                "Radius: " + this.radius +"\n" +
+                "Area: " + this.getArea()+ "\n" +
+                "Perimeter: " + this.getPerimeter() + "\n" +
+                "Diameter: " + this.getDiameter());
+    }
 }
