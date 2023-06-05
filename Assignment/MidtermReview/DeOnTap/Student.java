26fc7b979cbc174e90d2817291c91eac27a3654d
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-05 16:05:38 +0700

    [AS4] [Task4] add object student

---
+++ Assignment/MidtermReview/DeOnTap/Student.java
@@ -0,0 +1,44 @@
+package MidtermReview.DeOnTap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Student {
+    private String name;
+    private double avgScore;
+    private String studentId;
+
+    public Student(){}
+    public Student(String name, double avgScore, String studentId){
+        this.studentId = studentId;
+        this.name = name;
+        this.avgScore = avgScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAvgScore() {
+        return avgScore;
+    }
+
+    public void setAvgScore(double avgScore) {
+        this.avgScore = avgScore;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+}
+
+
